0b007234f596bbf3fd08fe7a6ec6e9df0356730c
jpgarrahan <43605232+jpgarrahan@users.noreply.github.com>
2023-10-12 07:12:49 +0100

    uploaded existing webpage

---
+++ Seminars_files/Seminars.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20210226
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Seminars_files/SeminarsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
